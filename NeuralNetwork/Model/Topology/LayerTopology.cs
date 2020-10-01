f70cd948ce87a69820e595f239c3fe174ad28ad3
ShaTalKiN <kornasevich@gmail.com>
2020-10-01 17:28:36 +0300

    test

---
+++ NeuralNetwork/Model/Topology/LayerTopology.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeuralNetwork
+{
+    internal class LayerTopology
+    {
+        internal int NeuronsCount { get; private set; }
+
+        internal Func<double, double> ActivationFunction { get; private set; }
+
+        public LayerTopology(int neuronsCount, Func<double, double> activationFunction)
+        {
+            NeuronsCount = neuronsCount;
+            ActivationFunction = activationFunction;
+        }
+    }
+}
